702b5a35e27321b484f6df017a69d36b9449f834
IUT Informatique <iutinfo@debian>
2022-05-30 17:30:23 +0200

    Documentation Subject and WaitingList

--- src/main/Subject.java
+++ src/main/Subject.java
@@ -18,6 +18,14 @@ public class Subject {
     private int id;
     private CalculAffectation<Student> calcul;
 
+    
+    /**
+     * Crée une matière (subject) entièrement spécifiée avec son nombre maximum d'étudiants pour le tutorat, son nom, son ID et
+     * l'attribution d'une liste de tuteurs et d'une liste de tutorés
+     * @param MAX_STUDENT : Nombre maximum d'étudiants tuteurs et tutorés compris
+     * @param NAME : Nom de la matière
+     * @param id : ID de la matière
+     */
     public Subject(int MAX_STUDENT, String NAME,int id) {
         this.MAX_STUDENT = MAX_STUDENT;
         this.NAME = NAME;
@@ -26,52 +34,103 @@ public class Subject {
         this.id=id;
     }
 
+    /**
+     * Renvoie l'ID de la matière demandée
+     * @return ID de la matière
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Renvoie le professeur en charge de la matière demandée
+     * @return inCharge donc le professeur en question
+     */
     public Teacher getInCharge(){
         return inCharge;
     }
 
+    /**
+     * Permet d'attribuer / de mofifier le professeur en charge de la matière demandée
+     * @param inCharge donc un professeur
+     */
     public void setInCharge(Teacher inCharge){
         if(inCharge.isInCharge(this)){
             this.inCharge = inCharge;
         }
     }
 
+    /**
+     * Renvoie le nombre maximum d'étudiants pour le tutorat de la matière demandée défini à la création de la matière
+     * @return MAX_STUDENT
+     */
     public int getMAX_STUDENT() {
         return this.MAX_STUDENT;
     }
 
+    /**
+     * Renvoie le nom de ma matière demandée
+     * @return name soit le nom de la matière
+     */
     public String getNAME() {
         return this.NAME;
     }
 
+    /**
+     * Renvoie la liste des étudiants qui sont des tutorés
+     * @return tutoredList soit l'ArrayList des tutorés
+     */
     public ArrayList<Tutored> getTutoredList() {
         return this.tutoredList;
     }
 
+    /**
+     * Renvoie la liste des étudiants qui sont des tuteurs
+     * @return tutorList soit l'ArrayList des tuteurs
+     */
     public ArrayList<Tutor> getTutorList() {
         return this.tutorList;
     }
 
+    /**
+     * Vérifie si la liste des tutorés contient un tutoré donné en paramètre
+     * @param t : un tutoré
+     * @return true si le tutoré fait bien partie de la liste des tutorés, false sinon
+     */
     public boolean contains(Tutored t){
         return tutoredList.contains(t);
     }
 
+    /**
+     * Vérifie si la liste des tuteurs contient un tuteur donné en paramètre
+     * @param t : un tuteur
+     * @return true si le tuteur fait bien partie de liste des tuteurs, false sinon
+     */
     public boolean contains(Tutor t){
         return tutorList.contains(t);
     }
 
+    /**
+     * Renvoie le calcul d'affectation de Student
+     * @return calcul qui est un calcul d'affectation des graphes
+     */
     public CalculAffectation<Student> getCalcul() {
         return calcul;
     }
 
+    /**
+     * Permet d'attribuer / modifier le calcul d'affectation de Students
+     * @param calcul : un calcul d'affectation de Student
+     */
     public void setCalcul(CalculAffectation<Student> calcul) {
         this.calcul = calcul;
     }
 
+    /**
+     * Vérifie si l'étudiant t est un tuteur ou un tutoré ou aucun des deux
+     * @param t : un étudiant (type Student)
+     * @return true si l'étudiant t est bien un tuteur ou un tutoré, false sinon
+     */
     public boolean contains(Student t){
         if(t instanceof Tutored)        return contains((Tutored) t);
         else if(t instanceof Tutor)     return contains((Tutor) t);
@@ -79,10 +138,19 @@ public class Subject {
     }
 
     @Override
+    /**
+     * Renvoie le nom de la matière demandée
+     * @return NAME soit le nom de la matière
+     */
     public String toString() {
         return NAME;
     }
 
+    /**
+     * Renvoie le tuteur qui a été affecté au tutoré donné en paramètre
+     * @param tutored : un tutoré
+     * @return le tuteur (Tutor) qui est affecté au tutoré et null si il n'y en a pas 
+     */
     public Tutor getAffectation(Tutored tutored){
         for (Arete<Student> a : getCalcul().getAffectation()) {
             if(a.getExtremite1().equals((Student) tutored)){
@@ -94,6 +162,11 @@ public class Subject {
         return null;
     }
 
+    /**
+     * Renvoie la liste des tutorés qui ont été affectés au tuteur donné en paramètre
+     * @param tutor : un tuteur
+     * @rerurn res : l'ArrayList des tutorés qui ont été affectés 
+     */
     public ArrayList<Tutored> getAffectation(Tutor tutor){
         ArrayList<Tutored> res = new ArrayList<>();
         for (Arete<Student> a : getCalcul().getAffectation()) {
--- src/main/WaitingList.java
+++ src/main/WaitingList.java
@@ -11,58 +11,110 @@ public class WaitingList {
     private ArrayList<Tutor> tutor;
     private ArrayList<Tutored> tutored; 
 
+    /**
+     * Crée une liste d'attente (WaitingList) pour une matière donnée en paramètre, composée d'une liste de tuteurs et d'une liste de tutorés
+     * @param subject : une matière
+     */
     public WaitingList(Subject subject){
         tutored = new ArrayList<>();
         tutor=new ArrayList<>();
         this.subject=subject;
     }
 
+    /**
+     * Crée une liste d'attente (WaitingList) pour une matière donnée en paramètre, composée d'une liste de tuteurs et d'une liste de tutorés données en paramètres également
+     * @param subject : une matière
+     * @param tutored : une ArrayList de tutorés
+     * @param tutor : une ArrayList de tuteurs
+     */
     public WaitingList(Subject subject, ArrayList<Tutored> tutored, ArrayList<Tutor> tutor){
         this.tutored =tutored ;
         this.tutor = tutor;
         this.subject=subject;
     }
 
+    /**
+     * Ajoute un tuteur à l'ArrayList des tuteurs
+     * @param t : un tuteur
+     */
     public void addTutor(Tutor t){
         tutor.add(t);
     }
 
+    /**
+     * Ajoute un tutoré à l'ArrayList des tutorés
+     * @param t : un tutoré
+     */
     public void addTutored(Tutored t){
         tutored.add(t);
     }
 
+    /**
+     * Vérifie si le tutoré donné en paramètre fait partie de l'ArrayList Tutored
+     * @param t : un tutoré
+     * @return true si le tutoré fait bien parti de l'ArrayList des tutorés, false sinon
+     */
     public boolean contains(Tutored t){
         return tutored.contains(t);
     }
 
+    /**
+     * Vérifie si le tuteur donné en paramètre fait partie de l'ArrayList Tutor
+     * @param t : un tuteur
+     * @return true si le tuteur fait bien partie de l'ArrayList des tuteurs, false sinon
+     */
     public boolean contains(Tutor t){
         return tutor.contains(t);
     }
 
+    /**
+     * Vérifie si le Student t est un tuteur ou un tutoré ou aucun des deux
+     * @param t : un étudiant (type Student)
+     * @return true si l'étudiant t est bien un tuteur ou un tutoré, false sinon
+     */
     public boolean contains(Student t){
         if(t instanceof Tutored)        return contains((Tutored) t);
         else if(t instanceof Tutor)     return contains((Tutor) t);
         else return false;
     }
 
-
+    /**
+     * Renvoie la matière concernée par la file d'attente
+     * @return subject soit la matière en question
+     */
     public Subject getSubject() {
         return this.subject;
     }
 
+    /**
+     * Modifie la matière de la file d'attente
+     * @param subject : une nouvelle matière
+     */
     public void setSubject(Subject subject) {
         this.subject = subject;
     }
 
+    /**
+     * Renvoie la liste des tuteurs
+     * @return tutor soit l'ArrayList des tuteurs en file d'attente
+     */
     public ArrayList<Tutor> getTutor() {
         return this.tutor;
     }
 
+    /**
+     * Renvoie la liste des tutorés
+     * @return tutored soit l'ArrayList des tutorés en file d'attente
+     */
     public ArrayList<Tutored> getTutored() {
         return this.tutored;
     }
 
     @Override
+    /**
+     * Renvoie une chaîne de caractère composée d'une matière en file d'attente avec sa liste de tuteurs attentes et sa liste de tutorés en attentes
+     * @return res soit la chaîne de caractères en question
+     */
     public String toString() {
         String res = subject.toString() + "\nTuteurs :\n" +tutor.toString() + "\nTutorés :\n"+ tutored.toString();
         return res;
