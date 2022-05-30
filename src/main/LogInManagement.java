1605eff013a84d4cf6d1ba72b379072320892287
Sacha Debusschère <sacha.debusschere.etu@univ-lille.fr>
2022-05-30 19:58:33 +0200

    Documentation LogInManagement and Level

--- src/main/LogInManagement.java
+++ src/main/LogInManagement.java
@@ -10,13 +10,25 @@ import main.Util.exceptions.WrongLoginException;
 public class LogInManagement {
     private Person logged;
 
+    /**
+     * Crée un état de connexion en prenant en compte la personne connectée (logged) en paramètre
+     * @param logged : montre qui est la personne connectée
+     */
     public LogInManagement(Person logged) {
         this.logged = logged;
     }
+    
+    /** 
+     * Crée un état de connexion sans paramètre qui instancie logged à null
+     */
     public LogInManagement() {
         this.logged =null;
     }
 
+    /**
+     * Renvoie l'état de connexion sous forme de chaîne de caractères
+     * @return logged (de type Person) si la personne est connectée et "null" sinon
+     */
     @Override
     public String toString() {
         if(isLogged()){
@@ -26,6 +38,12 @@ public class LogInManagement {
         }
     }
 
+    /**
+     * Interaction avec l'utilisateur lui demandant son login et son mot de passe avec un BufferedReader et renvoie ensuite la personne (Person) qui correspond
+     * @param p : ArrayList de personnes
+     * @param br : BufferedReader
+     * @return  la personne qui correspond aux login et mot de passe et WrongLoginException si elle ne correspond pas
+     */
     private Person getUserPwd(ArrayList<Person> p, BufferedReader br) throws IOException, WrongLoginException {
         System.out.println("Login:");
         String login = br.readLine();
@@ -41,6 +59,12 @@ public class LogInManagement {
         return res;
     }
 
+    /**
+     * "Connecte" l'utilisateur qui correspond aux login et mot de passe et lui signale
+     * @param p : l'ArrayList de personnes
+     * @param br : un BufferedReader
+     * @return logged soit la personne maintenant connectée
+     */
     public Person connect(ArrayList<Person> p, BufferedReader br) {
         boolean flag = true;
         while (flag) {
@@ -57,10 +81,18 @@ public class LogInManagement {
         return logged;
     }
 
+    /**
+     * Vérifie si l'utilisateur est connecté
+     * @return true si il est connecté (logged est non null) et false sinon
+     */
     public boolean isLogged(){
         return logged != null;
     }
 
+    /**
+     * Renvoie qui est la personne connectée
+     * @return logged soit la personne
+     */
     public Person getLogged() {
         return logged;
     }
--- src/main/Users/Level.java
+++ src/main/Users/Level.java
@@ -1,5 +1,8 @@
 package main.Users;
 
+/**
+ * Enum des différents niveaux qui correspondent aux années d'études pour le BUT
+ */
 public enum Level {
     first,second,third;
     
