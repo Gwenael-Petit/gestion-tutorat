c0ac4ef33cb34795c338af78763b1effb24ad35a
Gwenael Petit <gwenael.petit.etu@univ-lille.fr>
2022-06-03 19:50:45 +0200

    doc Graph.java

--- src/main/Util/Graph.java
+++ src/main/Util/Graph.java
@@ -12,10 +12,26 @@ import main.Users.Tutored;
 import main.Subject;
 import main.Users.Level;
 
-
+/**
+ * Class permetant de generer des graphes et de calculer des affectations en foctions de ces graphes
+ * Les graphes sont generer a partir de criteres que l'on peut activer et desactiver
+ */ 
 public abstract class Graph {
+
+    /**
+     * Coefficient que l'on applique pour fixer des affcetions manuellement
+     */
     private static int idx = 5000;
 
+    /**
+     * Permet de creer un graphe a partir de deux listes et d'une matiere
+     * @param tutore : Liste de tutores
+     * @param tuteurs : Liste de tuteurs
+     * @param subjectID : Entier identifiant une matiere
+     * @return gragh : graphe reliant tuteurs et tutores
+     * @see Tutor
+     * @see Tutored
+     */
     public static GrapheNonOrienteValue<Student> createGraph(ArrayList<Tutored> tutore, ArrayList<Tutor> tuteurs,
             int subjectID) {
 
@@ -46,6 +62,12 @@ public abstract class Graph {
         return graph;
     }
 
+    /**
+     * Permet de trier une liste de tutores donnee dans une matiere en fonctions des absences
+     * Cela permet d'activer le critere des absences pour les affectations
+     * @param tutore : Liste de tutores
+     * @param subjectID : Entier identifiant une matiere
+     */
     public static void turnOnAbsence(ArrayList<Tutored> tutore, int subjectID) {
         for (int i = 0; i < tutore.size(); i++) {
             if (!tutore.get(i).getFixed()[subjectID]) {
@@ -60,6 +82,12 @@ public abstract class Graph {
         Collections.sort(tutore);
     }
 
+    /**
+     * Permet de trier une liste de tutores donnee dans une matiere en excluant les absences
+     * Cela permet de desactiver le critere des absences pour les affectations
+     * @param tutore : Liste de tutores
+     * @param subjectID : Entier identifiant une matiere
+     */
     public static void turnOffAbsence(ArrayList<Tutored> tutore, int subjectID) {
         for (int i = 0; i < tutore.size(); i++) {
             if (!tutore.get(i).getFixed()[subjectID]) {
@@ -73,6 +101,12 @@ public abstract class Graph {
         Collections.sort(tutore);
     }
 
+    /**
+     * Permet de trier une liste de tuteur donnee dans une matiere en fonctions de la moyenne de premiere annee
+     * Cela permet d'activer le critere de moyenne de premiere annee pour les affectations
+     * @param tutor : Liste de tuteurs
+     * @param subjectID : Entier identifiant une matiere
+     */
     public static void turnOnMoyPremiere(ArrayList<Tutor> tutor, int subjectID) {
         for (int i = 0; i < tutor.size(); i++) {
             if (!tutor.get(i).getFixed()[subjectID]) {
@@ -86,6 +120,12 @@ public abstract class Graph {
         Collections.sort(tutor);
     }
 
+    /**
+     * Permet de trier une liste de tuteur donnee dans une matiere en excluant la moyenne de premiere annee
+     * Cela permet de desactiver le critere de moyenne de premiere annee pour les affectations
+     * @param tutor : Liste de tuteurs
+     * @param subjectID : Entier identifiant une matiere
+     */
     public static void turnOffMoyPremiere(ArrayList<Tutor> tutor, int subjectID) {
         for (int i = 0; i < tutor.size(); i++) {
             if (!tutor.get(i).getFixed()[subjectID]) {
@@ -99,6 +139,14 @@ public abstract class Graph {
         Collections.sort(tutor);
     }
 
+    /**
+     * Permet de fixer un couple de tuteur et tutore se trouvant dans les listes pour une matiere donnee
+     * @param tutoreList : Liste de tutores
+     * @param tuteurList : Lsite de tuteurs
+     * @param tutore : entier identifiant un tutore dans la liste
+     * @param tuteurs : entier identifiant un tuteur dans la liste
+     * @param subjectID: entier identifiant une matiere
+     */
     public static void fixCouple(ArrayList<Student> tutoreList, ArrayList<Student> tuteurList, int tutore, int tuteur,
             int subjectID) {
         // On fixe les couples de notre choix. On ne peux pas fixer un couple avec un
@@ -116,6 +164,16 @@ public abstract class Graph {
         }
     }
 
+    /**
+     * Permet de calculer les affectations pour former des couples de tuteurs et de tutores pour une matiere donnee
+     * Passage par une creation de graphe pour calculer les meilleurs affectations
+     * @param tutores : Liste de tutores
+     * @param tuteurs : Liste de tuteurs
+     * @param subjetcs : Liste de matiere
+     * @param subjectID : entier identifiant la matiere dans la liste
+     * @return calcul : Calcul d'affectation d'un graphe et de deux listes
+     * @see CalculeAffectation
+     */
     public static CalculAffectation<Student> compute(ArrayList<Tutored> tutores, ArrayList<Tutor> tuteurs, ArrayList<Subject> subjects, int subjectID) {
         int idx=0;
         while(tutores.size()!=tuteurs.size()){
