7b2cc1c454890800b55731aab761b8447e0dbfbb
Sacha Debusschere <sacha.debusschere.etu@univ-lille.fr>
2022-06-03 18:47:54 +0200

    Documentation of Tutor and Tutored

--- src/main/Users/Tutor.java
+++ src/main/Users/Tutor.java
@@ -6,40 +6,81 @@ import java.util.Map;
 
 import main.Subject;
 
+/**
+ * Classe qui cree un tuteur à partir d'un Student
+ */
 public class Tutor extends Student implements Cloneable{
-    private Map<Subject,ArrayList<Tutored>> tutored;    
+    // Une Map associant une ArrayList de tutores a une matiere
+	private Map<Subject,ArrayList<Tutored>> tutored;    
 
+	/**
+	 * Constructeur d'un Tutor entirement specifie et ajout de la Map composee de matieres et d'ArrayList des tutores a la Map de base
+	 * @param nom : le nom du tuteur
+	 * @param prenom : le prenom du tuteur
+	 * @param login : le login du tuteur
+	 * @param password : le mot de passe du tuteur
+	 * @param moyenne : la moyenne du tuteur
+	 * @param annee : l'annee de scolarite du tuteur
+	 * @param tutored : l'ArrayList des tutores
+	 */
     public Tutor(String nom, String prenom, String login, String password, double[] moyenne, String annee,Map<Subject,ArrayList<Tutored>> tutored) {
         super(nom, prenom, login, password, moyenne, annee);
         this.tutored = tutored;
     }
 
+    /**
+     * Constructeur d'un Tutor (sans son login) et ajout de la Map composee de matieres et d'ArrayList des tutores a la Map de base
+     * @param nom : le nom du tuteur
+     * @param prenom : le prenom du tuteur
+     * @param password : le mot de passe du tuteur
+     * @param moyenne : la moyenne du tuteur
+     * @param annee : l'annee de scolarite du tuteur
+     * @param tutored : l'ArrayList des tuteurs
+     */
     public Tutor(String nom, String prenom, String password, double[] moyenne, String annee,Map<Subject,ArrayList<Tutored>> tutored) {
         super(nom, prenom, prenom+"."+nom, password, moyenne, annee);
         this.tutored = tutored;
     }
 
+    /**
+     * Constructeur d'un Tutor (sans son login) et instanciation d'une HashMap associant une matiere a une liste des tutores a la Map de base
+     * @param nom
+     * @param prenom
+     * @param password
+     * @param moyenne
+     * @param annee
+     */
     public Tutor(String nom, String prenom, String password, double[] moyenne, String annee) {
         super(nom, prenom, password, moyenne, annee);
         this.tutored = new HashMap<Subject,ArrayList<Tutored>>();
     }
 
+    /**
+     * Renvoie la Map des ArrayList de tutores pour chaque matiere
+     * @return tutored : la Map 
+     */
     public Map<Subject, ArrayList<Tutored>> getTutored() {
         return tutored;
     }
 
+    /**
+     * Verifie si l'ajout d'une matiere a la Map est possible et le fait si oui
+     * @param s : une matiere
+     * @return true si la matiere a ete ajoute a la Map et false sinon
+     */
     public boolean addSubject(Subject s){
         tutored.put(s, new ArrayList<Tutored>());
         return true;
     }
 
+    /**
+     * Renvoie un clone d'un tuteur, dans le cas où on voudrait l'affecter a plusieurs tutores par exemple
+     */
     public Tutor clone() throws CloneNotSupportedException{
         return (Tutor) super.clone();
     }
 
-    /*
-
-    public boolean addTutored(Subject s, Tutored t){
+    /* public boolean addTutored(Subject s, Tutored t){
         if(tutored.get(s)==null){
             return false;
         }
@@ -51,7 +92,9 @@ public class Tutor extends Student implements Cloneable{
         return true;
     }*/
 
-    @Override
+    /**
+     * Renvoie la chaine d'informations sur un tuteur
+     */
     public String toString() {
         return super.toString();
     }
--- src/main/Users/Tutored.java
+++ src/main/Users/Tutored.java
@@ -5,32 +5,64 @@ import java.util.Map;
 
 import main.Subject;
 
-
+/**
+ * Classe qui cree un tutore a partir d'un Student
+ */
 public class Tutored extends Student{
-    private Map<Subject,Tutor> tutor;    
+    // Map qui associe une ArrayList de tuteurs a une matiere
+	private Map<Subject,Tutor> tutor;    
 
+	/**
+	 * Constructeur d'un Tutored entierement specifie et ajout la Map composee de matieres et d'ArrayList des tuteurs a la Map de base
+	 * @param nom
+	 * @param prenom
+	 * @param login
+	 * @param password
+	 * @param moyenne
+	 * @param annee
+	 * @param tutor
+	 */
     public Tutored(String nom, String prenom, String login, String password, double[] moyenne, String annee,Map<Subject,Tutor> tutor) {
         super(nom, prenom, login, password, moyenne, annee);
         this.tutor = tutor;
     }
 
+    /**
+     * Constructeur d'un Tutored (sans son login) et ajout de la Map composee de matieres et d'ArrayList des tuteurs a la Map de base
+     * @param nom
+     * @param prenom
+     * @param password
+     * @param moyenne
+     * @param annee
+     * @param tutor
+     */
     public Tutored(String nom, String prenom, String password, double[] moyenne, String annee,Map<Subject,Tutor> tutor) {
         super(nom, prenom, password, moyenne, annee);
         this.tutor = tutor;
     }
 
+    /**
+     * Constructeur d'un Tutor (sans son login) et ajout de la Map composee de matieres et instanciation d'une HashMap qui associe une matiere a une ArrayList de tuteurs
+     * @param nom
+     * @param prenom
+     * @param password
+     * @param moyenne
+     * @param annee
+     */
     public Tutored(String nom, String prenom, String password, double[] moyenne, String annee) {
         super(nom, prenom, password, moyenne, annee);
         this.tutor = new HashMap<Subject,Tutor>();
     }
 
+    /**
+     * Renvoie la Map qui associe une ArrayList de tuteurs par matiere
+     * @return
+     */
     public Map<Subject, Tutor> getTutor() {
         return tutor;
     }
 
-    /*
-
-    public boolean addTutor(Subject s, Tutor t){
+    /* public boolean addTutor(Subject s, Tutor t){
         if(tutor.get(s)==null){
             return false;
         }
@@ -42,7 +74,9 @@ public class Tutored extends Student{
         return true;
     }*/
 
-    @Override
+    /**
+     * Renvoie les informations sur le tutore
+     */
     public String toString() {
         return super.toString();
     }
